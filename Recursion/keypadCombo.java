52287c76d80c95a9f1220760bc2dab51791218af
ShreyasP77 <80884270+ShreyasP77@users.noreply.github.com>
2022-06-28 13:48:47 +0530

    Update keypadCombo.java

--- Recursion/keypadCombo.java
+++ Recursion/keypadCombo.java
@@ -1,6 +1,7 @@
 public class keyPadcombo {
     public static String[] keypad = {".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
     public static void printCombo(String str,int idx,String combo){
+       
         if (idx == str.length()){
             System.out.println(combo);
             return;
@@ -69,4 +70,43 @@ public class keyPadcombo {
 // Mapping of current str[1] i.e 3 --> ghi
 // Inside the loop of 'ghi' string and the current index is i..e character = i
 // fi
+// 
+// 
+// 
+// 
+// // Leetcode problem
+// class Solution {
+           
+//     public List<String> letterCombinations(String digits) {
+//         List<String> combo = new ArrayList<>();
+//         String letters2 = "";
+//         // len
+//         letterCombo(digits,0,combo,"");
+//         return combo;
+            
+//     }
 
+//     public static String[] keypad = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}; //leters from numbers ranging from 2-9.
+//     public static void letterCombo(String digits,int idx, List<String> combo,String letters){
+//         if(digits.length()==0){
+//             combo.remove(String.valueOf(""));
+//               return;
+//           }
+//         if(idx == digits.length()){
+              
+//             combo.add(letters);
+//                 return;
+        
+//         }
+    
+          
+//          char curChar = digits.charAt(idx);
+//         String mapping = keypad[curChar-'2'];
+
+//         for(int i=0; i<mapping.length();i++){
+//            letterCombo(digits,idx+1,combo,letters+mapping.charAt(i));
+
+//         }
+        
+//     }
+// }
