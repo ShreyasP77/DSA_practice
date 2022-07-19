7dbb442b6307fb0fc33b04c46520b522116efdf2
ShreyasP77 <80884270+ShreyasP77@users.noreply.github.com>
2022-07-19 22:00:01 +0530

    Update and rename checkIfStringsValidPallindromeOrNot.java to checkIfStringsValidPallindromeII.java

---
+++ Strings/checkIfStringsValidPallindromeII.java
@@ -0,0 +1,27 @@
+       private boolean isPalindrome(String s,int i,int j){
+                 while(i<j){
+                    if(s.charAt(i)==s.charAt(j)){
+                            i++;
+                            j--;
+                    }else{
+                            return false;
+                    }
+            }
+            return true;
+        }
+    public boolean validPalindrome(String s) {
+        
+            int i=0;
+            int j=s.length()-1;
+            while(i<j){
+                    if(s.charAt(i)==s.charAt(j)){
+                            i++;
+                            j--;
+                    }else{
+                            return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+                    }
+            }
+            return true;
+        
+    }
+       
