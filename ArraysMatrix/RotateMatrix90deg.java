958acf41a9d7c714a6b8507d9f48b37063168656
ShreyasP77 <80884270+ShreyasP77@users.noreply.github.com>
2023-02-27 13:56:44 +0530

    Update RotateMatrix90deg.java

--- ArraysMatrix/RotateMatrix90deg.java
+++ ArraysMatrix/RotateMatrix90deg.java
@@ -9,12 +9,24 @@ class Solution {
             }
         }
         
+//         This is to mirror the matrix or flip thwe matrix according to requirement.
+//         Below is the case of vertical flip.
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix.length/2;j++){
                 int temp = 0;
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[i][matrix.length-1-j];
                  matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+        
+//           Below is the case of horizontal flip.
+        for(int i=0;i<matrix.length/2;i++){
+            for(int j=0;j<matrix.length;j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length-1-i][j];
+                 matrix[matrix.length-1-i][j] = temp;
             }
         }
         
